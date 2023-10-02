0b57bce66390f8d8071609d24c59d465fa1f1991
CarlosCastanon2099 <108638686+CarlosCastanon2099@users.noreply.github.com>
2023-10-02 17:42:43 -0600

    01-10-2023 Programa-02 - BBIndiceEsp Core Completado

--- Programas/Programa-02/src/beta.py
+++ Programas/Programa-02/src/beta.py
@@ -1,24 +1,24 @@
 def busqueda_binaria_indice_especial(lista):
     def busqueda_especial(i_izq, i_der):
         if i_izq == i_der:
-            if lista[i_izq] == i_izq:
+            if lista[i_izq - 1] == i_izq:
                 return i_izq
             else:
                 return None
         else:
             mitad = (i_izq + i_der) // 2
-            if lista[mitad] < mitad:
+            if lista[mitad - 1] < mitad:
                 return busqueda_especial(mitad + 1, i_der)
             else:
                 return busqueda_especial(i_izq, mitad)
 
-    return busqueda_especial(0, len(lista) - 1)
+    return busqueda_especial(1, len(lista))
 
 # Ejemplo de uso
 lista_ordenada = [-5, -4, -2, 0, 1, 3, 5, 7, 8, 9, 11, 13, 14, 15, 16]
 indice_especial = busqueda_binaria_indice_especial(lista_ordenada)
 
 if indice_especial is not None:
-    print(f"Índice especial encontrado en el índice {indice_especial}: {lista_ordenada[indice_especial]}")
+    print(f"Indice especial encontrado en el indice {indice_especial}: {lista_ordenada[indice_especial - 1]}")
 else:
-    print("No se encontró ningún índice especial en la lista.")
+    print("No se encontro ningun indice especial en la lista.")
