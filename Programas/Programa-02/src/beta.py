a85673e8c40d3a0812ccacfa90cf2f62708b6fe6
CarlosCastanon2099 <108638686+CarlosCastanon2099@users.noreply.github.com>
2023-10-02 20:05:47 -0600

    01-10-2023 Programa-02 - BBIndiceEsp toString casi Completado

--- Programas/Programa-02/src/beta.py
+++ Programas/Programa-02/src/beta.py
@@ -1,18 +1,27 @@
 def busqueda_binaria_indice_especial(lista):
+    print(f"Espacio de búsqueda inicial: {lista}")
     def busqueda_especial(i_izq, i_der):
+        iteraciones = 0
+        print(f"Iteracion {iteraciones}: Espacio de busqueda: {lista[i_izq - 1:i_der]}")
         if i_izq == i_der:
             if lista[i_izq - 1] == i_izq:
-                return i_izq
+                return i_izq, 1
             else:
-                return None
+                return -1, 1
         else:
             mitad = (i_izq + i_der) // 2
             if lista[mitad - 1] < mitad:
-                return busqueda_especial(mitad + 1, i_der)
+                indice, iteraciones = busqueda_especial(mitad + 1, i_der)
             else:
-                return busqueda_especial(i_izq, mitad)
+                indice, iteraciones = busqueda_especial(i_izq, mitad)
+            return indice, iteraciones + 1
+            
+    indice_especial, iteraciones = busqueda_especial(1, len(lista))
 
-    return busqueda_especial(1, len(lista))
+    #print(f"Índice especial encontrado en el índice {indice_especial}: {lista[indice_especial - 1]}" )
+    print(f"Número total de iteraciones: {iteraciones}")
+    return indice_especial
+
 
 # Ejemplo de uso
 lista_ordenada = [-5, -4, -2, 0, 1, 3, 5, 7, 8, 9, 11, 13, 14, 15, 16]
