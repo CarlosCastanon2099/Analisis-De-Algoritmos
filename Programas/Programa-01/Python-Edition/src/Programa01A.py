de2255279dd4015143e84791cee7a376f7f4cef1
CarlosCastanon2099 <108638686+CarlosCastanon2099@users.noreply.github.com>
2023-12-26 22:57:11 -0600

    26-12-2023 P-01 Completado

--- Programas/Programa-01/Python-Edition/src/Programa01A.py
+++ Programas/Programa-01/Python-Edition/src/Programa01A.py
@@ -13,12 +13,16 @@ cnt = 0
 arr = [[0 for i in range(128)] for j in range(128)]
 
 
+
 def place(x1, y1, x2, y2, x3, y3):
     global cnt
     cnt += 1
     arr[x1][y1] = cnt
     arr[x2][y2] = cnt
     arr[x3][y3] = cnt
+
+
+
 
 
 def tile(n, x, y):
@@ -31,6 +35,7 @@ def tile(n, x, y):
             for j in range(n):
                 if arr[x + i][y + j] == 0:
                     arr[x + i][y + j] = cnt
+
         return 0
     for i in range(x, x + n):
         for j in range(y, y + n):
@@ -80,10 +85,10 @@ def plot_grid(grid, colors, special):
             ax.add_patch(rect)
 
     plt.gca().invert_yaxis()  # Invert y-axis to match the array indexing
-    plt.show()
+    #plt.show()  Descomenta esto si deseas veer TODAS las imagenes generadas por el programa (son muchas) en ventanas pop-out
 
 
-size_of_grid = 8
+size_of_grid = 2
 a = 0
 b = 0
 arr[a][b] = -1
@@ -136,6 +141,133 @@ save_grid(arr, color_map, (a, b), 'adoquinamientoFinal.png')
 def potenciadeDos(n):
     return 2 ** n
 
+
+# Funcion auxiliar que genera un archivo .png de la imagen actual (similar a la funcion save_grid)
+def generate_image():
+    global cnt
+    color_map = {0: 'white', -1: 'black'}
+
+    for i in range(1, cnt + 1):
+        color_map[i] = plt.cm.get_cmap('tab20')(i / cnt)  # Use 'tab20' colormap
+
+    ## Display the grid on the console
+    #for i in range(size_of_grid):
+    #    for j in range(size_of_grid):
+    #        print(arr[i][j], end=" ")
+    #    print()
+
+    # Plot the grid using matplotlib
+    plot_grid(arr, color_map, (a, b))
+
+    # Save the grid as a PNG image
+    save_grid(arr, color_map, (a, b), 'adoquinamiento' + str(cnt) + '.png')
+
+# Funcion que usa a generate_image() para generar un gif usando todas las imagenes generadas por generate_image() 
+# una vez generado el gif se borran todas las imagenes generadas por generate_image()
+def generate_gif():
+    global cnt
+    images = []
+    # guardamos todas las imagenes generadas por generate_image() en una lista, tengamos en cuenta que el nombre de las imagenes
+    # generadas por generate_image() es 'adoquinamiento' + str(cnt) + '.png' por lo que podemos usar un ciclo for para recorrer
+    # todas las imagenes generadas que cumplan con ese patron de nombre (es decir, podria no existir el archivo adoquinamiento4.png pero si
+    # existir el archivo adoquinamiento5.png, por lo que el ciclo for solo recorrera los archivos que existan)
+    for filename in glob.glob('adoquinamiento*.png'):
+        images.append(imageio.imread(filename))
+    
+    # Ahora ordenamos las imagenes que se hayan generado por generate_image() para que se muestren en orden
+    for filename in sorted(glob.glob('adoquinamiento*.png'), key=os.path.getmtime):
+        images.append(imageio.imread(filename))
+
+    # Generamos el gif usando las imagenes que se hayan generado por generate_image()
+    imageio.mimsave('adoquinamientoPasoaPaso.gif', images)
+
+    # Borramos todas las imagenes generadas por generate_image()
+    for filename in glob.glob('adoquinamiento*.png'):
+        os.remove(filename)
+
+
+
+# Ahora tenemos el metodo tile2 el cual funciona exactamente igual que el metodo tile, pero en este caso lo modificaremos para
+# que genere una imagen por cada llamada recursiva que haga, de esta manera, al finalizar el programa, tendremos una imagen
+# por cada llamada recursiva que se hizo, y asi podremos ver como se va formando el adoquinamiento
+def tile2(n, x, y):
+    global cnt
+    r = 0
+    c = 0
+    if n == 2:
+        cnt += 1
+        generate_image()
+        for i in range(n):
+            for j in range(n):
+                if arr[x + i][y + j] == 0:
+                    arr[x + i][y + j] = cnt
+        generate_image()
+        return 0
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if arr[i][j] != 0:
+                r = i
+                c = j
+    generate_image()
+    if r < x + n / 2 and c < y + n / 2:
+        place(x + int(n / 2), y + int(n / 2) - 1, x + int(n / 2), y + int(n / 2), x + int(n / 2) - 1, y + int(n / 2))
+
+    elif r >= x + int(n / 2) and c < y + int(n / 2):
+        place(x + int(n / 2) - 1, y + int(n / 2), x + int(n / 2), y + int(n / 2),
+              x + int(n / 2) - 1, y + int(n / 2) - 1)
+
+    elif r < x + int(n / 2) and c >= y + int(n / 2):
+        place(x + int(n / 2), y + int(n / 2) - 1, x + int(n / 2), y + int(n / 2),
+              x + int(n / 2) - 1, y + int(n / 2) - 1)
+
+    elif r >= x + int(n / 2) and c >= y + int(n / 2):
+        place(x + int(n / 2) - 1, y + int(n / 2), x + int(n / 2), y + int(n / 2) - 1,
+              x + int(n / 2) - 1, y + int(n / 2) - 1)
+
+    tile2(int(n / 2), x, y + int(n / 2))
+    tile2(int(n / 2), x, y)
+    tile2(int(n / 2), x + int(n / 2), y)
+    tile2(int(n / 2), x + int(n / 2), y + int(n / 2))
+
+    return 0
+
+
+size_of_grid = 2
+a = 0
+b = 0
+arr[a][b] = -1
+tile2(size_of_grid, 0, 0)
+
+# Mapping of numbers to colors
+color_map = {0: 'white', -1: 'black'}
+
+for i in range(1, cnt + 1):
+    color_map[i] = plt.cm.get_cmap('tab20')(i / cnt)  # Use 'tab20' colormap
+
+# Display the grid on the console
+#for i in range(size_of_grid):
+#    for j in range(size_of_grid):
+#        print(arr[i][j], end=" ")
+#    print()
+
+# Plot the grid using matplotlib
+#plot_grid(arr, color_map, (a, b))
+
+#save_grid(arr, color_map, (a, b), 'adoquinamientoFinal.png')
+
+
+
+# Funcion auxiliar que ajusta los fps de un gif
+def adjust_gif_fps(filename, fps):
+    reader = imageio.get_reader(filename)
+    fps = float(fps)
+    writer = imageio.get_writer('adoquinamientoPasoaPaso.gif', fps=fps)
+    for im in reader:
+        writer.append_data(im)
+    writer.close()
+
+
+
 # Use of the program: python3 Programa01A.py <n> 
 # where <n> is the size of the grid (must be a power of 2)
 # Example: python3 Programa01A.py 8
@@ -148,7 +280,7 @@ if __name__ == '__main__':
             a = 0
             b = 0
             arr[a][b] = -1
-            tile(size_of_grid, 0, 0)
+            tile2(size_of_grid, 0, 0)
 
             # Mapping of numbers to colors
             color_map = {0: 'white', -1: 'black'}
@@ -166,7 +298,16 @@ if __name__ == '__main__':
             plot_grid(arr, color_map, (a, b))
 
             # Save the grid as a PNG image
-            save_grid(arr, color_map, (a, b), 'adoquinamientoFinal.png')
+            save_grid(arr, color_map, (a, b), 'ElAdoquinamientoFinal.png')
+
+            # Generamos el Gif 
+            generate_gif()
+
+            # Ajustamos los fps del gif
+            adjust_gif_fps('adoquinamientoPasoaPaso.gif', 1)
+
+
+
         else:
             print('Error: <n> must be a power of 2')
     else:
